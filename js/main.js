d9115cc42ddfca2568d7fbe7279bfe74cade57d0
MoMerzak <57521637+MoMerzak@users.noreply.github.com>
2024-10-06 15:04:56 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
 console.log('Main loaded');
 let playerScore = 0;
 let computerScore = 0;
+let playerCredits = 0;
+let computerCredits = 0;
+let currentDiceSum = 0;
  
 const goButton = document.getElementById("go-button");
 const lowerButton = document.getElementById("lower-button");
 const higherButton = document.getElementById("higher-button");
 const resultDisplay = document.getElementById("result-display");
+const playerCreditsDisplay = document.querySelector(".player-credits");
+const goText = document.querySelector(".message-box");
  
-let currentDiceSum = 0;
+const computerCreditsDisplay = document.querySelector('.computer-credits');
+ 
  
 goButton.addEventListener('click', function() {
     console.log('Go button clicked');
-    let diceOne = Math.floor(Math.random() * 6) + 1;
-    let diceTwo = Math.floor(Math.random() * 6) + 1;
-    currentDiceSum = diceOne + diceTwo;
-   
-    console.log("Dobbelsteen 1:", diceOne);
-    console.log("Dobbelsteen 2:", diceTwo);
+    goButton.remove();
+    goText.remove();
+ 
+    currentDiceSum = 0;
+    for (let i = 0; i < 2; i++) {
+        const diceValue = Math.floor(Math.random() * 6) + 1;
+        currentDiceSum += diceValue;
+        console.log(`Dobbelsteen ${i + 1}:`, diceValue);
+    }
+ 
     console.log("Totaal:", currentDiceSum);
     resultDisplay.textContent = `Totaal: ${currentDiceSum}`;
 });
@@ -38,17 +48,25 @@ function checkGuess(guess) {
  
         if (guess === 'lower' && randomNumber < currentDiceSum) {
             playerScore++;
+            playerCredits++;
             console.log("Je hebt gewonnen!");
             resultDisplay.textContent += ` Je hebt gewonnen met: ${randomNumber}`;
         } else if (guess === 'higher' && randomNumber > currentDiceSum) {
             playerScore++;
+            playerCredits++;
             console.log("Je hebt gewonnen!");
             resultDisplay.textContent += ` Je hebt gewonnen met: ${randomNumber}`;
         } else {
             computerScore++;
+            computerCredits++;
             console.log("Je hebt verloren.");
             resultDisplay.textContent += ` Je hebt verloren met: ${randomNumber}`;
         }
+        playerCreditsDisplay.textContent = playerCredits;
+        computerCreditsDisplay.textContent = computerCredits;
+ 
+        console.log("Player Credits:", playerCredits);
+        console.log("Computer Credits:", computerCredits);
     } else {
         console.log("Klik eerst op de Go-knop!");
     }
